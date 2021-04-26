9d600709a33b4403ce83ecf54faacf9602e72626
ip19 <ipoberezhnyi@gmail.com>
2021-04-26 18:46:36 +0300

    secret_access_key fix

--- src/index.js
+++ src/index.js
@@ -14,5 +14,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//       Pm/xxHe7GOz5Ah3rJXjkCDnE3chza/8EYyfouvPZ
+// AKIASQCU22YZK56ZMN3R
+// SbBwFvKBoZjL27bXcgToJopGESo+toc5PmW/gbh7
 reportWebVitals();
